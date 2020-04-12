853afd41cada613f61ae8ee941d28f3532724baa
Communist217 <Needforspeed1900@gmail.com>
2020-04-12 18:49:20 +0700

    update

--- crawler.js
+++ crawler.js
@@ -1,19 +1,20 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const search = 'blackberry';
+const search = 'vsmart';
 const productList = [];
 
 request(`https://www.thegioididong.com/tim-kiem?key=` + search , (error, response, html) => {
   if (!error && response.statusCode === 200) {
       const $ = cheerio.load(html);
       $('.listsearch').find('li').each((i, element) => {
-        product = { productName: '', productImg: '', priceTag: '' };
+        var product = { productName: '',  priceTag: '' , productPromo: ''};
         product.productName = $(element).find('h3').text().trim();
-        product.productImg = $(element).find('img').attr('src');
         product.priceTag = $(element).find('strong').text().trim();
-  
+        product.productPromo = $(element).find('.promo').children('p').text().trim();
+        
         if (product.productName != '' && product.priceTag != '') {
+          console.log(product)
           productList.push(product);
         }
       });
@@ -21,8 +22,8 @@ request(`https://www.thegioididong.com/tim-kiem?key=` + search , (error, respons
       const csvWriter = createCsvWriter({
         header: [
           {id: 'productName', title: 'Tên'},
-          {id: 'productImg', title: 'Hình ảnh'},
           {id: 'priceTag', title: 'Giá bán'},
+          {id: 'productPromo', title: 'Khuyến mại'},
         ],
         path: 'listSearch.csv',
         encoding: 'ascii',
