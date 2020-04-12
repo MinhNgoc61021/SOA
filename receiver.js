25f6a913776672e0480c44f08964a7f880fd286f
Communist217 <Needforspeed1900@gmail.com>
2020-04-13 00:19:22 +0700

    update

--- receiver.js
+++ receiver.js
@@ -1,4 +1,5 @@
 const amqp = require('amqplib/callback_api');
+const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
@@ -6,10 +7,11 @@ function Crawler(error, response, html) {
       if (!error) {
           let $ = cheerio.load(html);
           var film = { title: '', releaseDate: '', rating: '' }
-          film.title = $('.title_wrapper').children('h1').text();
-          film.releaseDate = $('#titleYear').children('a').text();
-          film.rating = $('.ratingValue').children('strong').children('span').attr('itemprop', 'ratingValue').text();
-          fs.appendFile(`/fileList/${film.title}.json`, film , function (err) {
+          film.title = $('.title_wrapper').children('h1').text().trim();
+          film.releaseDate = $('#titleYear').children('a').text().trim();
+          film.rating = $('.ratingValue').children('strong').children('span').attr('itemprop', 'ratingValue').text().trim();
+          console.log(film);
+          fs.writeFile(`filmList/${film.title}.json`, JSON.stringify(film) , function (err) {
             if (err) throw err;
             console.log('Updated!');
           });
@@ -17,11 +19,11 @@ function Crawler(error, response, html) {
 }
 
 amqp.connect('amqp://localhost', function(err, conn) {
-    conn.createChannel(function(err, ch) {
+    conn.createChannel(function(err, channel) {
         // declare queue again in case the 
         var queue = 'URL';
-        ch.assertQueue(queue, {durable: false});
-        ch.consume(q, function(msg) {
+        channel.assertQueue(queue, {durable: false});
+        channel.consume(queue, function(msg) {
             var url = msg.content.toString()
             request(url, Crawler)
         }, { noAck: true });
--- sender.js
+++ sender.js
@@ -2,22 +2,21 @@ var amqp = require('amqplib/callback_api');
 
 const getMovieByIndex = (index) => {
   index += 137523;
-  return `https://www.imdb.com/title/tt0/${index}`;
+  return `https://www.imdb.com/title/tt0${index}`;
 }
 
 // Connect to the RabbitMQ server
-amqp.connect('amqp://localhost:5672', function(err, conn) {
+amqp.connect('amqp://localhost', function(err, conn) {
  // Creating a channel, where the api for getting data
-  conn.createChannel(function(err, ch) {
+  conn.createChannel(function(err, channel) {
     // declaring the queue 
     var queue = 'URL';
-    ch.assertQueue(queue, {durable: false});
+    channel.assertQueue(queue, {durable: false});
     for (var i = 0; i < 150; i++) {
       var url= getMovieByIndex(i);
-      ch.sendToQueue(queue, new Buffer(url));
+      channel.sendToQueue(queue, Buffer.from(url));
       console.log('Sent: ' + url);
     };
   });
   // We close the connection and exit
-  setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
