4828a5fa0e076bd989745739dd324599b3efe272
Communist217 <Needforspeed1900@gmail.com>
2020-04-14 21:50:59 +0700

    update

--- receiver.js
+++ receiver.js
@@ -31,6 +31,7 @@ async function receiver() {
      // Create queue
     var queue = 'URL';
     await channel.assertQueue(queue, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
     await channel.consume(queue, function(msg) {
                     console.log(" Received: ", msg.content.toString());
                     var url = msg.content.toString();            
